ff35cddca38f6f99412c1818fab5e24b7c55ca7c
Michael Faughn <mike.faughn@gmail.com>
2020-05-29 21:38:44 -0400

    more of the same

--- lib/github.rb
+++ lib/github.rb
@@ -4,13 +4,18 @@ def github_test(name = nil, email = nil)
   repo_dir = File.expand_path('~/projects/v2-pubs-test')
   name ||= 'Michael Faughn'
   email ||= 'mike.faughn@gmail.com'
-  system "cd #{repo_dir}"
-  system "git config user.email #{email} && git config user.name '#{name}'"
-  system "git checkout -b #{name}"
-  system "touch test.txt"
-  t = Time.now.utc
-  system "echo 'Maps to commit by #{name} with email #{email} at #{t}' >> test.txt"
-  system "git add ."
-  system "git commit -m 'test commit message at #{t}'"
-  puts system 'git push -u origin ' + name
+  branch_name = name.gsub(/\s/, '')
+  # here = __dir__
+  Dir.chdir(repo_dir) {
+    system "cd #{repo_dir}"
+    system "git config user.email #{email} && git config user.name '#{name}'"
+    system "git checkout -b #{branch_name}"
+    system "touch test.txt"
+    t = Time.now.utc
+    system "echo 'Maps to commit by #{name} with email #{email} at #{t}' >> test.txt"
+    system "git add test.txt"
+    system "git commit -m 'test commit message at #{t}'"
+    puts system 'git push -u origin ' + branch_name    
+    system 'git checkout master'
+  }
 end
--- lib/v2web/custom_spec/hl7_spec.rb
+++ lib/v2web/custom_spec/hl7_spec.rb
@@ -1,10 +1,16 @@
 collection(:Summary, HL7::DataType) do
-  order('abbreviation', 'name', 'definition', 'withdrawn', :to_beginning => true)
+  order('code', 'name', 'definition', 'withdrawn', :to_beginning => true)
   # hide(html_source) # FIXME crashes page when this is on...WTF?!?!?!
 end
 
 collection(:Summary, HL7::SegmentDefinition) do
-  order('abbreviation', 'name', 'description', 'withdrawn', 'origin', :to_beginning => true)
+  order('code', 'name', 'description', 'withdrawn', 'origin', :to_beginning => true)
+  hide('source')
+  relabel('origin', 'From Chapter')
+end
+
+organizer(:Details, HL7::Message) do
+  order('code', 'name', 'description', 'withdrawn', 'origin', :to_beginning => true)
   hide('source')
   relabel('origin', 'From Chapter')
 end
